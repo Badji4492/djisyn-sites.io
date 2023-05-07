ecfbc3806310adc6faaafcb51cfe20cad6691115
Badji4492 <sesebadji@gmail.com>
2023-05-07 11:37:42 +0200

    ajout commentaire js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+// Bouton burger
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('nav');
 burger.addEventListener('click', function(){
     burger.classList.toggle('open')
     nav.classList.toggle('open')
 })
-
+// bouton me contacter
 const contact = document.querySelector(".button-contact");
 contact.addEventListener('click', function(){
     window.location.href="contact.html";
 })
+// boutons reservations
 const bouton = document.querySelectorAll(".button-resa");
 bouton.forEach(bouton=>{
 bouton.addEventListener('click',function(){
